72cc427cdc4e480b37c39fc99640110d449806c1
RenardElectric <102425097+RenardElectric@users.noreply.github.com>
2022-12-24 00:18:04 +0100

    progressbig progress

---
+++ script/lang/fr.js
@@ -0,0 +1,26 @@
+function translate() {
+    document.body.innerHTML = document.body.innerHTML
+    .replace('head.home', 'Maison')
+    .replace('head.maps', 'Maps')
+    .replace('head.news', 'Nouvelles')
+    .replace('head.language', 'Language')
+
+
+    .replace('creations.new_creations', 'Nouvelles Maps')
+    .replace('creations.other_creations', 'Les autres Maps')
+    .replace('creations.header_left_1', 'minecraft version')
+    .replace('creations.header_right_1', 'numbers of players')
+    .replace('creations.creation_name_1', 'map name')
+    .replace('creations.creation_desc_1', 'map short desc')
+
+    .replace('creations.header_left_2', 'minecraft version')
+    .replace('creations.header_right_2', 'numbers of players')
+    .replace('creations.creation_name_2', 'map name')
+    .replace('creations.creation_desc_2', 'map short desc')
+
+    .replace('creations.header_left_3', 'minecraft version')
+    .replace('creations.header_right_3', 'numbers of players')
+    .replace('creations.creation_name_3', 'map name')
+    .replace('creations.creation_desc_3', 'map short desc')
+    
+}
