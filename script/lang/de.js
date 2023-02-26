9ea83b821d3549b93ccf2d3fa1c3f05937a1adc0
RenardElectric <102425097+RenardElectric@users.noreply.github.com>
2023-02-26 14:20:26 +0100

    new translation keys

--- script/lang/de.js
+++ script/lang/de.js
@@ -9,19 +9,19 @@ function translate() {
     .replace('creations.new_creations', 'New Creations')
     .replace('creations.other_creations', 'Other Creations')
     
-    .replace('creations.header_left_1', 'minecraft version')
-    .replace('creations.header_right_1', 'numbers of players')
-    .replace('creations.creation_name_1', 'creation name')
-    .replace('creations.creation_desc_1', 'creation short desc')
+    .replace('creation_1.header.left', 'minecraft version')
+    .replace('creation_1.header.right', 'numbers of players')
+    .replace('creation_1.name', 'creation name')
+    .replace('creation_1.desc', 'creation short desc')
 
-    .replace('creations.header_left_2', 'minecraft version')
-    .replace('creations.header_right_2', 'numbers of players')
-    .replace('creations.creation_name_2', 'creation name')
-    .replace('creations.creation_desc_2', 'creation short desc')
+    .replace('creation_2.header.left', 'minecraft version')
+    .replace('creation_2.header.right', 'numbers of players')
+    .replace('creation_2.name', 'creation name')
+    .replace('creation_2.desc', 'creation short desc')
 
-    .replace('creations.header_left_3', 'minecraft version')
-    .replace('creations.header_right_3', 'numbers of players')
-    .replace('creations.creation_name_3', 'creation name')
-    .replace('creations.creation_desc_3', 'creation short desc')
+    .replace('creation_3.header.left', 'minecraft version')
+    .replace('creation_3.header.right', 'numbers of players')
+    .replace('creation_3.name', 'creation name')
+    .replace('creation_3.desc', 'creation short desc')
     
 }
